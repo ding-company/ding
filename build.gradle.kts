b603e7267896d0b309a242e709efaa4af56e7b43
jing kwan jung <joey@jings-MacBook-Air.local>
2024-08-25 18:04:06 +0900

    chore: skeleton 폴더 지정

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,17 +23,31 @@ dependencies {
     // detekt
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${Dependency.detektVersion}")
 }
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = Dependency.targetJvmVersion
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = Dependency.targetJvmVersion
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+    register<Copy>("copyDirectory") {
+        from("skeleton")
+        into("src/main/kotlin/in/ding")
+        val appName = project.findProperty("appName") as String? ?: "app"
+        doLast {
+            val copiedFolder = file("src/main/kotlin/in/ding/app")
+            if (copiedFolder.exists()) {
+                val renamedFolder = file("src/main/kotlin/in/ding/$appName")
+                copiedFolder.renameTo(renamedFolder)
+            }
+        }
     }
 }
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
 
 detekt {
     autoCorrect = true
