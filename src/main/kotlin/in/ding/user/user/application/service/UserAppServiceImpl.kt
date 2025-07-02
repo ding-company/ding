6d632cab00e015d76ed81f09b14fdc2ee92ea836
joey <dingwan0331@gmail.com>
2025-07-02 18:05:02 +0900

    refactor: user domain

--- src/main/kotlin/in/ding/user/user/application/service/UserAppServiceImpl.kt
+++ src/main/kotlin/in/ding/user/user/application/service/UserAppServiceImpl.kt
@@ -4,23 +4,25 @@ import `in`.ding.user.user.application.dto.command.UserRegisterCommand
 import `in`.ding.user.user.application.dto.consumer.UserRegisterByCustomerEventCommand
 import `in`.ding.user.user.domain.UserRepository
 import `in`.ding.user.user.domain.model.User
+import `in`.ding.user.user.domain.model.enumerate.ContactType
 import `in`.ding.user.user.domain.model.enumerate.UserNationality
 
 class UserAppServiceImpl(private val repository: UserRepository) :
     UserAppService {
     override fun register(command: UserRegisterCommand) {
-        val user = User.register(
-            phoneNumber = command.phoneNumber,
-            email = command.email,
-            nationality = command.nationality
-        )
-        repository.save(user)
+        throw NotImplementedError()
+//        val user = User.register(
+//            phoneNumber = command.phoneNumber,
+//            email = command.email,
+//            nationality = command.nationality
+//        )
+//        repository.save(user)
     }
     override fun registerFromCustomer(command: UserRegisterByCustomerEventCommand) {
         val user = User.register(
             exKey = command.exKey,
-            phoneNumber = command.phoneNumber,
-            email = null,
+            contact = command.phoneNumber!!,
+            contactType = ContactType.PHONE_NUMBER,
             nationality = UserNationality.KR
         )
         repository.save(user)
