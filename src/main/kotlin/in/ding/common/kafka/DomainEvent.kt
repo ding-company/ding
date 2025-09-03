8edebccfa08220f01b9cf5d15e1b48fc1a16d11d
joey <dingwan0331@gmail.com>
2025-09-03 23:49:36 +0900

    feat: event id 추가

--- src/main/kotlin/in/ding/common/kafka/DomainEvent.kt
+++ src/main/kotlin/in/ding/common/kafka/DomainEvent.kt
@@ -1,9 +1,12 @@
 package `in`.ding.common.kafka
 
 import java.time.LocalDateTime
+import java.util.UUID
 
 interface DomainEvent {
     val eventName: String
     val eventType: EventType
+    val eventId: UUID
     val occurredAt: LocalDateTime
+    val version: EventVersion
 }
--- src/main/kotlin/in/ding/user/auth/domain/event/AuthEvent.kt
+++ src/main/kotlin/in/ding/user/auth/domain/event/AuthEvent.kt
@@ -1,5 +1,15 @@
 package `in`.ding.user.auth.domain.event
 
 import `in`.ding.common.kafka.DomainEvent
+import `in`.ding.common.kafka.EventType
+import `in`.ding.common.kafka.EventVersion
+import java.time.LocalDateTime
+import java.util.UUID
 
-interface AuthEvent : DomainEvent
+abstract class AuthEvent(
+    override val eventName: String,
+    override val eventType: EventType,
+    override val eventId: UUID = UUID.randomUUID(),
+    override val occurredAt: LocalDateTime = LocalDateTime.now(),
+    override val version: EventVersion = EventVersion.V1
+) : DomainEvent
