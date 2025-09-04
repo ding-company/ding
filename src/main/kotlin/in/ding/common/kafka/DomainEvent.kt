b0fa8bd17c4dc746544e4a16a30c85d52f971700
joey <dingwan0331@gmail.com>
2025-09-04 23:56:40 +0900

    remove: domain kafka config

--- src/main/kotlin/in/ding/common/kafka/DomainEvent.kt
+++ src/main/kotlin/in/ding/common/kafka/DomainEvent.kt
@@ -1,8 +1,12 @@
 package `in`.ding.common.kafka
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 import java.time.LocalDateTime
 import java.util.UUID
 
+// TODO application.yaml 안먹히는 이유 알아내야함
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 interface DomainEvent {
     val eventName: String
     val eventType: EventType
