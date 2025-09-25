f0b482a955ec08bdff037a0c0e29020c9d18c45d
joey <dingwan0331@gmail.com>
2025-09-26 01:45:56 +0900

    feat: otp issue 유저 컨슈머 추가

--- src/main/kotlin/in/ding/user/auth/application/handler/OtpIssuanceHandler.kt
+++ src/main/kotlin/in/ding/user/auth/application/handler/OtpIssuanceHandler.kt
@@ -6,15 +6,12 @@ import `in`.ding.user.auth.domain.model.OtpSession
 import `in`.ding.user.auth.domain.model.vo.OtpCode
 import `in`.ding.user.auth.domain.repository.RedisOtpRepository
 import `in`.ding.user.auth.infrastructure.messaging.kafka.AuthEventPublisher
-import `in`.ding.user.user.domain.UserRedisRepository
 import `in`.ding.user.user.domain.model.ContactPolicy
-import `in`.ding.user.user.domain.model.User
 import org.springframework.stereotype.Component
 
 @Component
 class OtpIssuanceHandler(
     private val otpRedisRepository: RedisOtpRepository,
-    private val userRedisRepository: UserRedisRepository,
     private val contactPolicy: ContactPolicy,
     private val publisher: AuthEventPublisher
 ) {
@@ -22,13 +19,7 @@ class OtpIssuanceHandler(
         val otpCode = OtpCode.generate()
         val otp = OtpSession.create(contact = event.contact, code = otpCode)
         val contactType = contactPolicy.determineContactType(nationality = event.nationality)
-        val user = User.makeTempUser(
-            contact = event.contact,
-            contactType = contactType,
-            nationality = event.nationality
-        )
         otpRedisRepository.saveOtp(contact = event.contact, otp = otp, OtpSession.getOtpTtl())
-        userRedisRepository.saveTemporaryUser(contact = event.contact, userData = user)
         publisher.publish(OtpIssuedEvent.of(otp, contactType))
     }
 }
