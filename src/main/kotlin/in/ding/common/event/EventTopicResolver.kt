04c65349f9d3dae1d6f2076529a8368dddda18ab
joey <dingwan0331@gmail.com>
2026-02-01 22:01:55 +0900

    feat: topic resolver 추가

---
+++ src/main/kotlin/in/ding/common/event/EventTopicResolver.kt
@@ -0,0 +1,17 @@
+package `in`.ding.common.event
+
+import org.springframework.stereotype.Component
+
+@Component
+class EventTopicResolver(
+    private val properties: EventTopicsProperties
+) {
+    fun resolve(context: EventContext): String =
+        when (context) {
+            EventContext.AUTH -> properties.auth
+            EventContext.USER -> properties.user
+            EventContext.TERM -> properties.term
+            EventContext.PAYMENT -> properties.payment
+            EventContext.CUSTOMER -> properties.customer
+        }
+}
